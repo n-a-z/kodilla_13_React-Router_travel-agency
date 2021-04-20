44efe4ddfd0b613449fe690b0fe95290876b1996
naz <naz@o2.pl>
2021-04-20 15:27:04 +0200

    Add missing views in App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
 // TODO - import other views
+import Trip from './components/views/Trip/TripContainer';
+import Countries from './components/views/Countries/CountriesContainer';
+import Country from './components/views/Country/CountryContainer';
+import Regions from './components/views/Regions/RegionsContainer';
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
@@ -38,11 +42,14 @@ class App extends React.Component {
       <BrowserRouter>
         <MainLayout>
           <Switch location={location}>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/trips' component={Trips} />
-            {/* TODO - add more routes for other views */}
-            <Route exact path='/info' component={Info} />
-            <Route path='*' component={NotFound} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/trips" component={Trips} />
+            <Route exact path="/trip/:id" component={Trip} />
+            <Route path="/countries" component={Countries} />
+            <Route path="/country/:id" component={Country} />
+            <Route path="/regions" component={Regions} />
+            <Route exact path="/info" component={Info} />
+            <Route path="*" component={NotFound} />
           </Switch>
         </MainLayout>
       </BrowserRouter>
